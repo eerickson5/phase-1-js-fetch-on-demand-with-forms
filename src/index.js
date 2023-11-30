df406c0dcbe77f83748e9b9e6935b0975575244a
Elise Erickson <elisece717@gmail.com>
2023-11-30 15:24:03 -0500

    adds db fetch using js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputValue = e.target.children[1].value;
+
+    fetch("http://localhost:3000/movies/"+inputValue)
+    .then(res => res.json())
+    .then(data => {
+        const title = document.querySelector("section#movieDetails h4");
+        title.textContent = data.title;
+        const summary = document.querySelector("section#movieDetails p");
+        summary.textContent = data.summary;
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
